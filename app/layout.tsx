c22d0f1a9c441ed8e1f5def7035a663219b112fe
kangSUJI <sjkang@yescnc.co.kr>
2024-07-07 22:51:27 +0900

    fix build

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import { Metadata } from "next";
-import Navigation from "../components/navigation";
+import Navigation from "../components/Navigation";
 import "../styles/global.css";
 
 export const metadata: Metadata = {
